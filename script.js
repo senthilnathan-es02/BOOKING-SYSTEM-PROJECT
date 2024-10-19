2ab00ba783a1b7e9c71bb6bba508248b13f6b568
senthilnathan-es02 <eswarensenthil@gmail.com>
2024-10-19 14:32:42 +0530

    login formet

---
+++ script.js
@@ -0,0 +1,11 @@
+// enter page login button click // 
+
+document.getElementById("enter-login-button").onclick = function() {
+    window.location.href="pages/login.html"; 
+};
+
+// enter page signin button click//
+
+document.getElementById("enter-signin-button").onclick =function () {
+    window.location.href="pages/signin.html";
+};
